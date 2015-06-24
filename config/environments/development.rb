52784322570fba527c65e47415f5eebc2339eecc
shershey@gmail.com <shershey@gmail.com>
2015-06-24 15:51:59 +0000

    Changed to us region

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -38,5 +38,5 @@ Shawnband::Application.configure do
     :access_key_id     => ENV['AWS_ACCESS_KEY_ID'],
     :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
   )
-  BUCKET='friedbananasalbx2015swingsongs'
+  BUCKET='friedbananasalbx2015swingsongsus'
 end
