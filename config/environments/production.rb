21e44470b97127858745cedcec3b7f3cc9f32168
Kevin Sihlanick <kevin.sihlanick@gmail.com>
2015-06-25 09:33:52 -0400

    fix bucket name

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -70,6 +70,6 @@ Shawnband::Application.configure do
     :access_key_id     => ENV['AWS_ACCESS_KEY_ID'],
     :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
   )
-  BUCKET='shawnbandcoolsongs'
+  BUCKET='shawnsbandcoolsongs'
   
 end
